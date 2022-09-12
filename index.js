6e923dbaca73124869c07d594bd179f316d811d2
prithvi chigurupati <prithvi.chigurupati76@gmail.com>
2022-09-12 18:39:14 +0530

    basic setup of player cards

---
+++ index.js
@@ -0,0 +1,55 @@
+function getDiceRollArray(diceCount) {
+    let newDiceRolls = [];
+    for (let i = 0; i < diceCount; i++) {
+        newDiceRolls.push(Math.floor(Math.random() * 6) + 1);
+    }
+    return newDiceRolls;
+    
+/* Challenge: 
+1. Replace the for loop in getDiceRollArray with a new Array() and fill it 
+with 0s as its initial state. The new array should be diceRoll length.
+2. Use .map() directly on the array rather than declaring any new variables 
+and return a random number from 1-6 in each element of the array.
+3. Make sure you delete anything from that function that we no longer need.
+*/       
+}
+
+function getDiceHtml(diceCount) {
+    return getDiceRollArray(diceCount).map(function(num){ 
+        return  `<div class="dice">${num}</div>`
+    }).join('')
+}
+
+const hero = {
+    elementId: "hero",
+    name: "Wizard",
+    avatar: "images/wizard.png",
+    health: 60,
+    diceCount: 3
+}
+
+const monster = {
+    elementId: "monster",
+    name: "Orc",
+    avatar: "images/orc.png",
+    health: 10,
+    diceCount: 1
+}
+
+function renderCharacter(data) {
+    const { elementId, name, avatar, health, diceCount } = data;
+    const diceHtml = getDiceHtml(diceCount)
+
+    document.getElementById(elementId).innerHTML =
+        `<div class="character-card">
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}" />
+            <div class="health">health: <b> ${health} </b></div>
+            <div class="dice-container">    
+                ${diceHtml}
+            </div>
+        </div>`;
+}
+
+renderCharacter(hero);
+renderCharacter(monster);
